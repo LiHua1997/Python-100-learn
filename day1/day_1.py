c3236fcffa062c63348b628a43e17bbf55f9741c
Li Hua <970607988@qq.com>
2020-05-10 20:45:55 +0800

    first day add 2 practic

---
+++ day1/day_1.py
@@ -0,0 +1,13 @@
+#练习1华氏转摄氏 C = (F - 32) / 1.8
+# f = float(input('please input f\n'))
+# c = (f - 32) / 1.8
+# print('输入的华氏度为： %.1f\n对应的摄氏度是： %.1f'%(f, c))
+# print(f'输入的华氏度为：{f:.1f}\n对应的摄氏度是：{c:.1f}')
+
+
+#练习2输入圆的半径输出圆的周长和面积
+
+r = float(input('please input r\n'))
+c = 3.1416 * r
+s = 3.1316 * r * r
+print(f'半径是：{r:.1f}\n周长是：{c:.1f}\n面积是：{s:.1f}')
