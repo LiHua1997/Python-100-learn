7c6f0a56594e9a535eadc47a3b88a0812854d3a0
Li Hua <970607988@qq.com>
2020-05-11 15:05:14 +0800

    add perfact answer

--- day5/day5_2.py
+++ day5/day5_2.py
@@ -1,9 +1,8 @@
 """
 完美数
-说明：完美数又称为完全数或完备数，
+找出1，9999之间的完美数
 它的所有的真因子（即除了自身以外的因子）的和（即因子函数）恰好等于它本身。
 例如：6（$6=1+2+3$）和28（$28=1+2+4+7+14$）就是完美数。
-完美数有很多神奇的特性，有兴趣的可以自行了解。
 
 Version: 0.1
 Author: St
@@ -16,3 +15,16 @@ for num in range(1,10001):
             factors_list.append(factor)
     if sum(factors_list) == num:
         print(num)
+#总结，时间复杂度太差
+# 答案
+# from math import sqrt
+#
+# for num in range(1, 10001):
+#     result = 0
+#     for factor in range(1, int(sqrt(num) + 1)):
+#         if num % factor == 0:
+#             result += factor
+#             if factor> 1 and num // factor != factor:
+#                 result += num//factor
+#     if result == num:
+#         print(num)
