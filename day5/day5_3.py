f2db37a04674f8e268565a0ed300f25d08b9d6f8
Li Hua <970607988@qq.com>
2020-05-11 19:40:17 +0800

    rename to Python-100-learn

--- day5/day5_3.py
+++ day5/day5_3.py
@@ -6,12 +6,13 @@
 Version = 0.1
 Author = St
 """
-import math
-for num in range(1, 101):#2-99range(2,100)
-    is_prime = True
-    for factor in range(2, int(math.sqrt(num) + 1)):
-        if num % factor == 0:
-            is_prime = False
-            continue #break
-    if is_prime:
-        print(num)#print(num, end = ' ')
+# import math
+# for num in range(1, 101):#2-99range(2,100)
+#     is_prime = True
+#     for factor in range(2, int(math.sqrt(num) + 1)):
+#         if num % factor == 0:
+#             is_prime = False
+#             continue #break
+#     if is_prime:
+#         print(num)#print(num, end = ' ')
+print(chr(97))
