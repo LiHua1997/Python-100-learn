af5471aea044f8a8c67dec0aec80809574a9b608
Li Hua <970607988@qq.com>
2020-05-11 15:19:09 +0800

    add practice prime

---
+++ day5/day5_3.py
@@ -0,0 +1,17 @@
+"""
+输出100以内的全部素数
+素数是只能被1和他自己整除的数
+例如：1，3，5
+
+Version = 0.1
+Author = St
+"""
+import math
+for num in range(1, 101):
+    is_prime = True
+    for factor in range(2, int(math.sqrt(num) + 1)):
+        if num % factor == 0:
+            is_prime = False
+            continue
+    if is_prime:
+        print(num)
