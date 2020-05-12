a6823f3e1593b3014e782279b1981697cdf56ea7
Li Hua <970607988@qq.com>
2020-05-12 21:57:52 +0800

    add yanghui answer

--- day7/day7_6.py
+++ day7/day7_6.py
@@ -1,8 +1,38 @@
-days_of_month = [
-    [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-][is_leap]
-print(days_of_month)
+"""
+杨辉三角
 
-print([[1], [2]][False])
-# result ： [1]
+Version： 0.1
+Author: St
+
+"""
+
+
+# def YangHui(rows):
+#     """
+#     生成杨辉三角
+#     :param rows: 生成行数
+#     :return:
+#     """
+#     t = []
+#     for i in range(rows):
+#         t.append([])
+#         for j in range(i+1):
+#             if j == 0 or j == i :
+#                 t[i].append(1)
+#             else:
+#                 t[i].append(t[i - 1][j - 1] + t[i - 1][j])
+#     return t
+
+def YangHui(num):
+    t = [[]] * num
+    for row in range(len(t)):
+        t[row] = [None] * int(row + 1)
+        for col in range(row+1):
+            if col == 0 or col == row:
+                t[row][col] = 1
+            else:
+                t[row][col] = t[row - 1][col] + t[row - 1][col - 1]
+    return t
+
+
+print(YangHui(5))
---
+++ day7/day7_7.py
@@ -0,0 +1,4 @@
+list = [[]] * 3
+print(list)
+list[0] = [None]*2
+print(list)
