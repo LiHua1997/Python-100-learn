b89fc4f3141153994ef52137b4232b7c29c287f6
Li Hua <970607988@qq.com>
2020-05-16 21:31:38 +0800

    add the clock

---
+++ day8/day8_1.py
@@ -0,0 +1,54 @@
+"""
+clock
+Version： 0.1
+Author: St
+"""
+import time
+import os
+
+
+class Clock(object):
+    """数字时钟"""
+    def __init__(self, hour=0, minute=0, second=0):
+        """
+        初始化方法
+        :param hour:时
+        :param minute:分
+        :param second:秒
+        """
+        self._hour = hour
+        self._minute = minute
+        self._second = second
+
+    def run(self):
+        """
+        模拟时钟运行
+        """
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
+                self._hour += 1
+                if self._hour == 24:
+                    self._hour = 0
+
+    def display(self):
+        """
+        显示时钟
+        """
+        print("%02d:%02d:%02d" % (self._hour, self._minute, self._second))
+#       return "%02d:%02d:%02d" %(self._hour, self._minute, self._second)
+
+def main():
+    clock_clock = Clock(13, 34, 50)
+    while True:
+        clock_clock.display()
+        time.sleep(1)
+        os.system('clear')
+        clock_clock.run()
+
+
+if __name__ == '__main__':
+    main()
